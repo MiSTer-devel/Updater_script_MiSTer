b75b98764d57ef0a067a80d6587ede789901da46
Locutus73 <alessandro.miele@gmail.com>
2018-12-16 16:58:45 +0100

    Version 1.3
    Added Kernel, Linux filesystem and bootloader updating functionality; added autoreboot option.

--- mister_updater.sh
+++ mister_updater.sh
@@ -15,6 +15,7 @@
 
 # Copyright 2018 Alessandro "Locutus73" Miele
 
+# Version 1.3 - 2018.12.16 - Added Kernel, Linux filesystem and bootloader updating functionality; added autoreboot option.
 # Version 1.2 - 2018.12.14 - Added support for distinct directories for computer cores, console cores, arcade cores and service cores; added an option for removing "Arcade-" prefix from arcade core names
 # Version 1.1 - 2018.12.11 - Added support for additional repositories (i.e. Scaler filters and Game Boy palettes), renamed some variables
 # Version 1.0 - 2018.12.11 - First commit
@@ -23,6 +24,10 @@
 
 #Change these self-explanatory variables in order to adjust destination paths, etc.
 MISTER_URL="https://github.com/MiSTer-devel/Main_MiSTer"
+#Comment next line if you don't want the Kernel, the Linux filesystem and the bootloader to be updated
+SD_INSTALLER_URL="https://github.com/MiSTer-devel/SD-Installer-Win64_MiSTer"
+UNRAR_DEBS_URL="http://http.us.debian.org/debian/pool/non-free/u/unrar-nonfree"
+TEMP_PATH="/tmp"
 BASE_PATH="/media/fat"
 declare -A CORE_CATEGORY_PATHS=(
 						["cores"]="$BASE_PATH/_Computer"
@@ -32,8 +37,10 @@ declare -A CORE_CATEGORY_PATHS=(
 					 )	
 DELETE_OLD_FILES=true
 REMOVE_ARCADE_PREFIX=true
+AUTOREBOOT=true
+REBOOT_PAUSE=5
 #Comment next line if you don't want to download from additional repositories (i.e. Scaler filters and Gameboy palettes) each time
-ADDITIONAL_REPOSITORIES=( "https://github.com/MiSTer-devel/Filters_MiSTer/tree/master/Filters txt $BASE_PATH/Filters" "https://github.com/MiSTer-devel/Gameboy_MiSTer/tree/master/palettes gbp $BASE_PATH/GameBoy/" )
+ADDITIONAL_REPOSITORIES=( "https://github.com/MiSTer-devel/Filters_MiSTer/tree/master/Filters txt $BASE_PATH/Filters" "https://github.com/MiSTer-devel/Gameboy_MiSTer/tree/master/palettes gbp $BASE_PATH/GameBoy" )
 
 
 
@@ -44,15 +51,21 @@ for CORE_DIR in "${CORE_CATEGORY_PATHS[@]}"; do
 	fi
 done
 
-CORE_URLS=$MISTER_URL$'\n'$(curl -ksLf "$MISTER_URL/wiki"| awk '/user-content-cores/,/user-content-development/' | grep -io '\(https://github.com/[a-zA-Z0-9./_-]*_MiSTer\)\|\(user-content-[a-z-]*\)')
+CORE_URLS=$SD_INSTALLER_URL$'\n'$MISTER_URL$'\n'$(curl -ksLf "$MISTER_URL/wiki"| awk '/user-content-cores/,/user-content-development/' | grep -io '\(https://github.com/[a-zA-Z0-9./_-]*_MiSTer\)\|\(user-content-[a-z-]*\)')
 CORE_CATEGORY="-"
+REBOOT_NEEDED=false
 
 for CORE_URL in $CORE_URLS; do
 	if [[ $CORE_URL == https://* ]]
 	then
 		echo "Checking $CORE_URL"
-		RELEASES_URL=https://github.com$(curl -ksLf "$CORE_URL" | grep -o '/MiSTer-devel/[a-zA-Z0-9./_-]*/tree/[a-zA-Z0-9./_-]*/releases' | head -n1)
-		RELEASE_URLS=$(curl -ksLf "$RELEASES_URL" | grep -o '/MiSTer-devel/[a-zA-Z0-9./_-]*_[0-9]\{8\}\w\?\(\.rbf\)\?')
+		if echo "$CORE_URL" | grep -q "SD-Installer"
+		then
+			RELEASES_URL="$CORE_URL"
+		else
+			RELEASES_URL=https://github.com$(curl -ksLf "$CORE_URL" | grep -o '/MiSTer-devel/[a-zA-Z0-9./_-]*/tree/[a-zA-Z0-9./_-]*/releases' | head -n1)
+		fi
+		RELEASE_URLS=$(curl -ksLf "$RELEASES_URL" | grep -o '/MiSTer-devel/[a-zA-Z0-9./_-]*_[0-9]\{8\}\w\?\(\.rbf\|\.rar\)\?')
 		
 		MAX_VERSION=""
 		MAX_RELEASE_URL=""
@@ -94,7 +107,7 @@ for CORE_URL in $CORE_URLS; do
 					if [[ "$MAX_VERSION" > "$CURRENT_LOCAL_VERSION" ]] && [ $DELETE_OLD_FILES == true ]
 					then
 						echo "Deleting $CURRENT_FILE"
-						rm "$CURRENT_FILE"
+						rm "$CURRENT_FILE" > /dev/null 2>&1
 					fi
 				fi
 			fi
@@ -108,8 +121,60 @@ for CORE_URL in $CORE_URLS; do
 			then
 				DESTINATION_FILE=$(echo "$MAX_RELEASE_URL" | sed 's/.*\///g' | sed 's/_[0-9]\{8\}[a-zA-Z]\{0,1\}//g')
 				echo "Copying $DESTINATION_FILE"
-				rm "$CURRENT_DIR/$DESTINATION_FILE"
+				rm "$CURRENT_DIR/$DESTINATION_FILE" > /dev/null 2>&1
 				cp "$CURRENT_DIR/$FILE_NAME" "$CURRENT_DIR/$DESTINATION_FILE"
+				REBOOT_NEEDED=true
+			fi
+			if echo "$CORE_URL" | grep -q "SD-Installer"
+			then
+				if [ ! -f "$BASE_PATH/unrar-nonfree" ]
+				then
+					UNRAR_DEB_URLS=$(curl -ksLf "$UNRAR_DEBS_URL" | grep -o '\"unrar[a-zA-Z0-9./_+-]*_armhf\.deb\"' | sed 's/\"//g')
+					MAX_VERSION=""
+					MAX_RELEASE_URL=""
+					for RELEASE_URL in $UNRAR_DEB_URLS; do
+						CURRENT_VERSION=$(echo "$RELEASE_URL" | grep -o '_[a-zA-Z0-9.+-]*_' | sed 's/_//g')
+						if [[ "$CURRENT_VERSION" > "$MAX_VERSION" ]]
+						then
+							MAX_VERSION=$CURRENT_VERSION
+							MAX_RELEASE_URL=$RELEASE_URL
+						fi
+					done
+					echo "Downloading $UNRAR_DEBS_URL/$MAX_RELEASE_URL"
+					curl -kL "$UNRAR_DEBS_URL/$MAX_RELEASE_URL" -o "$TEMP_PATH/$MAX_RELEASE_URL"
+					echo "Extracting unrar-nonfree"
+					ORIGINAL_DIR=$(pwd)
+					cd "$TEMP_PATH"
+					rm data.tar.xz > /dev/null 2>&1
+					ar -x "$TEMP_PATH/$MAX_RELEASE_URL" data.tar.xz
+					cd "$ORIGINAL_DIR"
+					rm "$TEMP_PATH/$MAX_RELEASE_URL"
+					tar -xJf "$TEMP_PATH/data.tar.xz" --strip-components=3 -C "$BASE_PATH" ./usr/bin/unrar-nonfree
+					rm "$TEMP_PATH/data.tar.xz"
+				fi
+				if [ -f "$BASE_PATH/unrar-nonfree" ] && [ -f "$CURRENT_DIR/$FILE_NAME" ]
+				then
+					sync
+					if $BASE_PATH/unrar-nonfree t "$CURRENT_DIR/$FILE_NAME"
+					then
+						echo ""
+						echo "======================================================================================"
+						echo "Hold your breath: updating the Kernel, the Linux filesystem, the bootloader and stuff."
+						echo "Stopping this will make your SD unbootable!"
+						echo ""
+						echo "If something goes wrong, please download the SD Installer from"
+						echo "$SD_INSTALLER_URL"
+						echo "and copy the content of the files/linux/ directory in the linux directory of the SD"
+						echo "======================================================================================"
+						echo ""
+						$BASE_PATH/unrar-nonfree e -y "$CURRENT_DIR/$FILE_NAME" files/linux/* $BASE_PATH/linux
+						$BASE_PATH/linux/updateboot
+						REBOOT_NEEDED=true
+					else
+						echo "Downloaded installer RAR is broken, deleting $CURRENT_DIR/$FILE_NAME"
+						rm "$CURRENT_DIR/$FILE_NAME" > /dev/null 2>&1
+					fi					
+				fi
 			fi
 			sync
 		else
@@ -119,6 +184,10 @@ for CORE_URL in $CORE_URLS; do
 		echo ""
 	else
 		CORE_CATEGORY=$(echo "$CORE_URL" | sed 's/user-content-//g')
+		if [ "$CORE_CATEGORY" == "" ]
+		then
+			CORE_CATEGORY="-"
+		fi
 	fi
 done
 
@@ -144,3 +213,14 @@ for ADDITIONAL_REPOSITORY in "${ADDITIONAL_REPOSITORIES[@]}"; do
 done
 
 echo "Done!"
+if [ $REBOOT_NEEDED == true ]
+then
+	if [ $AUTOREBOOT == true ]
+	then
+		echo "Rebooting in $REBOOT_PAUSE seconds"
+		sleep $REBOOT_PAUSE
+		reboot
+	else
+		echo "You should reboot"
+	fi
+fi
