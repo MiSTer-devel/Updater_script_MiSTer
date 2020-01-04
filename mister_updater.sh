87c9e0c7bc97fe2c430a597a3ab8befd4b6e9357
Grant King <grantek@users.noreply.github.com>
2020-01-04 14:13:42 +1100

    minor bug fixes to legacy option parsing

--- mister_updater.sh
+++ mister_updater.sh
@@ -220,10 +220,11 @@ then
 	echo "CORE_CATEGORY_DIRS"
 	echo "for backwards"
 	echo "compatibility."
-	unset $CORE_CATEGORY_DIRS
+	unset CORE_CATEGORY_DIRS
+	declare -A CORE_CATEGORY_DIRS
 	for idx in "${!CORE_CATEGORY_PATHS[@]}"
 	do
-		P=$CORE_CATEGORY_PATHS[idx]
+		P=${CORE_CATEGORY_PATHS[$idx]}
 		if [ "${P##${BASE_PATH}}" == "$P" ]
 		then
 			echo "Error interpreting"
@@ -232,7 +233,7 @@ then
 			echo "CORE_CATEGORY_DIRS"
 			exit 4
 		fi
-		CORE_CATEGORY_DIRS[idx]=${P##${BASE_PATH}}
+		CORE_CATEGORY_DIRS[$idx]=${P##${BASE_PATH}}
 	done
 fi
 if [ ! -z "$WORK_PATH" ]
@@ -320,12 +321,11 @@ if [[ -n "${NTP_SERVER}" ]] ; then
 	# (-b) force time reset, (-s) write output to syslog, (-u) use
 	# unprivileged port for outgoing packets to workaround firewalls
 	ntpdate -b -s -u "${NTP_SERVER}"
-    echo
 fi
 
 
 for P in "${CORE_CATEGORY_DIRS[@]}"; do
-  mkdir -p "${BASE_PATH}/${P}"
+	mkdir -p "${BASE_PATH}/${P}"
 done
 
 declare -A NEW_CORE_CATEGORY_DIRS
