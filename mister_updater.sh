9213aa86067e7cc350790d73298455145a3f6637
José Manuel Barroso Galindo <theypsilon@gmail.com>
2020-11-16 12:20:56 +0100

    Main branch for SEGASYS1

--- mister_updater.sh
+++ mister_updater.sh
@@ -530,6 +530,9 @@ function checkCoreURL {
 		*Minimig*)
 			RELEASES_URL="${CORE_URL}/file-list/MiSTer/releases"
 			;;
+		*SEGASYS1*)
+			RELEASES_URL="${CORE_URL}/file-list/main/releases"
+			;;
 		*)
 			RELEASES_URL="${CORE_URL}/file-list/master/releases"
 			;;
